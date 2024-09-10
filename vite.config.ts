686eec400526732d675f0897ecc17ad82cdf29b8
DmitrySinyapkin <dsinyapkin@mail.ru>
2024-09-10 15:34:20 +0300

    vk libs added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    port: 10888,
+  },
+  plugins: [
+    react(),
+    basicSsl(),
+  ],
 })
