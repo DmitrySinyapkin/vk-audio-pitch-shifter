9ee3377972dc3a59f4b1576702af5be5e668882f
DmitrySinyapkin <dsinyapkin@mail.ru>
2025-01-20 18:32:49 +0300

    added back button

--- src/panels/PlayerPanel.tsx
+++ src/panels/PlayerPanel.tsx
@@ -1,13 +1,31 @@
 import { useContext } from "react";
 import { PlayerContext } from "../context/PlayerContext";
-import { PanelHeader, Group } from "@vkontakte/vkui";
+import { PanelContext } from "../context/PanelContext";
+import { PanelHeader, PanelHeaderButton, Group } from "@vkontakte/vkui";
+import { Icon24BrowserBack } from '@vkontakte/icons'
 
 const PlayerPanel = () => {
-    const { source } = useContext(PlayerContext)
+    const { source, setSource } = useContext(PlayerContext)
+    const { setActivePanel } = useContext(PanelContext)
+
+    const onBackButtonClick = () => {
+        if (setActivePanel && setSource) {
+            setSource(undefined)
+            setActivePanel('main')
+        }
+    }
 
     return (
         <>
-            <PanelHeader>Player</PanelHeader>
+            <PanelHeader
+                before={
+                    <PanelHeaderButton onClick={onBackButtonClick}>
+                        <Icon24BrowserBack />
+                    </PanelHeaderButton>
+                }
+            >
+                Player
+            </PanelHeader>
             <Group>
                 <audio src={source} controls />
             </Group>
