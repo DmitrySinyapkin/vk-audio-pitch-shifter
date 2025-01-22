44341c274add94bdb9488d8329e28762f6b5cd09
DmitrySinyapkin <dsinyapkin@mail.ru>
2025-01-22 19:10:25 +0300

    refactoring

--- src/components/pitchShifter/PitchShifter.tsx
+++ src/components/pitchShifter/PitchShifter.tsx
@@ -1,6 +1,15 @@
-import { useContext, useState } from "react";
+import { ReactNode, useContext, useState } from "react";
 import { PlayerContext } from "../../context/PlayerContext";
 import { Slider, Flex, Text } from "@vkontakte/vkui";
+
+interface SliderItem {
+    min: number
+    max: number
+    step: number
+    value: number
+    onChange: (value: number) => void
+    description: ReactNode
+}
 
 const PitchShifter = () => {
     const { player } = useContext(PlayerContext)
@@ -18,28 +27,39 @@ const PitchShifter = () => {
         setPlaybackRate(value)
     }
 
+    const sliders: SliderItem[] = [
+        {
+            min: -12,
+            max: 12,
+            step: 1,
+            value: pitchOffset,
+            onChange: changePitch,
+            description: <Text>Изменение в полутонах: {pitchOffset > 0 ? `+${pitchOffset}` : pitchOffset}</Text>
+        },
+        {
+            min: 0.5,
+            max: 1.5,
+            step: 0.05,
+            value: playbackRate,
+            onChange: changePlaybackRate,
+            description: <Text>Темп: {Math.trunc(playbackRate * 100)}%</Text>
+        }
+    ]
+
     return (
         <Flex direction="column" align="center" margin="auto" gap='4xl'>
-            <Flex.Item flex="grow" style={{ width: '320px' }}>
-                <Slider
-                    min={-12}
-                    max={12}
-                    step={1}
-                    value={pitchOffset}
-                    onChange={changePitch}
-                />
-                <Text>Изменение в полутонах: {pitchOffset > 0 ? `+${pitchOffset}` : pitchOffset}</Text>
-            </Flex.Item>
-            <Flex.Item flex="grow" style={{ width: '320px' }}>
-                <Slider
-                    min={0.5}
-                    max={1.5}
-                    step={0.05}
-                    value={playbackRate}
-                    onChange={changePlaybackRate}
-                />
-                <Text>Темп: {Math.trunc(playbackRate * 100)}%</Text>
-            </Flex.Item>
+            {sliders.map((slider: SliderItem, index: number) => 
+                <Flex.Item key={index}flex="grow" style={{ width: '320px' }}>
+                    <Slider
+                        min={slider.min}
+                        max={slider.max}
+                        step={slider.step}
+                        value={slider.value}
+                        onChange={slider.onChange}
+                    />
+                    {slider.description}
+                </Flex.Item>
+            )}
         </Flex>
     )
 }
