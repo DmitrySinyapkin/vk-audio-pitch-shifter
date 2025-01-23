269983cbaf6eb6387792536e10c3a1e71427906b
DmitrySinyapkin <dsinyapkin@mail.ru>
2025-01-23 16:46:07 +0300

    updated vk-tunnel and added tunnel script

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 10888,
+    hmr: {
+      protocol: 'wss', 
+      host: 'localhost'
+    }
   },
   plugins: [
     react(),
