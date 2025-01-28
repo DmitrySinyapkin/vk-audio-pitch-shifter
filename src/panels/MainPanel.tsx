8b2928ad5eed75c24720e44d4bd6e9a62796c28a
DmitrySinyapkin <dsinyapkin@mail.ru>
2025-01-28 16:34:40 +0300

    added icon to panel header

--- src/panels/MainPanel.tsx
+++ src/panels/MainPanel.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import { PlayerContext } from "../context/PlayerContext";
 import { PanelContext } from "../context/PanelContext";
 import { PanelHeader, Group } from "@vkontakte/vkui";
+import { Icon24MusicOutline } from '@vkontakte/icons';
 import AudioUpload from "../components/main/AudioUpload";
 
 const MainPanel = () => {
@@ -16,7 +17,11 @@ const MainPanel = () => {
 
     return (
         <>
-            <PanelHeader>Audio Pitch Shifter</PanelHeader>
+            <PanelHeader
+                before={<Icon24MusicOutline fill="var(--vkui--color_icon_accent)" style={{ marginLeft: '16px' }} />}
+            >
+                Audio Pitch Shifter
+            </PanelHeader>
             <Group>
                 <AudioUpload />
             </Group>
